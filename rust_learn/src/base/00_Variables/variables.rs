ad02cf6ee7523f66aa50fadc2d204cea695c5c01
Martin <19776883@qq.com>
2024-02-23 17:58:38 +0800

    Update
    define learn rust language category.

---
+++ rust_learn/src/base/00_Variables/variables.rs
@@ -0,0 +1,21 @@
+/*
+    Binding and mutability
+        1. A variable can be used only if it has been initialized.
+        2. Use mut to mark a variable as mutable.
+  
+    Scope
+        A scope is the range within the program for which the item is valid.
+
+    Shadowing
+        You can declare a new variable with the same name as a previous variable,
+        here we can say the first one is shadowed by the second one.
+
+    Unused variables
+
+    Destructuring
+        We can use a pattern with let to destructure a tuple to separate variables.
+    
+    Destructuring assignments
+        
+
+*/
