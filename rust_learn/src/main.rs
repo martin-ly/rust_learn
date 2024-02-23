8ce580f81c577277bd63f67ee30a3cb5e9534979
Martin <19776883@qq.com>
2024-02-24 05:07:05 +0800

    Update

--- rust_learn/src/main.rs
+++ rust_learn/src/main.rs
@@ -1,8 +1,6 @@
-// Fix the error below with least amount of modification to the code
-fn main() {
-    let x: i32 = 5; // Uninitialized but used, ERROR !
-    //let y: i32; // Uninitialized but also unused, only a Warning !
 
-    assert_eq!(x, 5);
+use crate::language::base::c00_variables::uninitialized;
+fn main() {
+    uninitialized();
     println!("Success!");
 }
