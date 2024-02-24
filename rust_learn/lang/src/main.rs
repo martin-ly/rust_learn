5144022b6e33db6d7f3c3a26d93ebef87551635d
Martin <19776883@qq.com>
2024-02-25 02:46:36 +0800

    Update

--- rust_learn/lang/src/main.rs
+++ rust_learn/lang/src/main.rs
@@ -1,4 +1,5 @@
 
+
 fn used_function() {}
 
 //cargo  `#[allow(dead_code)]` is an attribute that disables the `dead_code` lint
@@ -6,7 +7,48 @@ fn used_function() {}
 fn unused_function() {}
 
 
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
     used_function();
     println!("Hello, world!");
+
+//------------------
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+
+    println!("The secret number is: {secret_number}");
+
+    println!("Please input your guess.");
+
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        //let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num)=>num,
+            Err(_)=>{
+                println!("Please type a number!");
+                continue;},
+        };
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {println!("You win!");break;},
+        }        
+    }
+
+
+
 }
