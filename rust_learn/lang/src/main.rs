f2e8b6b45dff3146c97f2d3af356765996998414
Martin <19776883@qq.com>
2024-02-24 19:57:20 +0800

    Update
    cargo reference

--- rust_learn/lang/src/main.rs
+++ rust_learn/lang/src/main.rs
@@ -1,3 +1,12 @@
+
+fn used_function() {}
+
+//cargo  `#[allow(dead_code)]` is an attribute that disables the `dead_code` lint
+#[allow(dead_code)]
+fn unused_function() {}
+
+
 fn main() {
+    used_function();
     println!("Hello, world!");
 }
